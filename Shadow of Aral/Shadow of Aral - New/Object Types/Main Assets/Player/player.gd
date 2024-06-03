cab9694fc527a1f50f20820cafcff34fd2e5e220
SJohnmith <smith795052@gmail.com>
2024-06-02 23:18:30 -0400

    Multiply By Scale to Account Fire Angle

--- Shadow of Aral/Shadow of Aral - New/Object Types/Main Assets/Player/player.gd
+++ Shadow of Aral/Shadow of Aral - New/Object Types/Main Assets/Player/player.gd
@@ -40,7 +40,7 @@ func player_movement(delta):
      
 # Update the Player Position and Direction
      move_and_slide()
-     $"Player Rig".update_facing_direction(direction)
+#     $"Player Rig".update_facing_direction(direction)
 
 # Handle Player Movement
 func player_action():
@@ -49,23 +49,27 @@ func player_action():
 
      # Gun Attached to Front Arm (Hard Coded the Path to Child Object Weapon)
      var muzzle_path = $"Player Rig".get_child(0).get_child(2).get_child(0).get_child(1).get_children()
-     var arm_path = $"Player Rig".get_child(0).get_child(2)
+     var front_arm_path = $"Player Rig".get_child(0).get_child(2)
      
-     var fire_direction = Vector2(cos(arm_path.rotation), sin(arm_path.rotation))
+     var arm_pointing_direction = Vector2(cos(front_arm_path.rotation), sin(front_arm_path.rotation))*($"Player Rig".scale)
      
      # Look Towards Mouse Position
-     var gun_direction = (get_global_mouse_position() - position).normalized()
+     var mouse_direction = (get_global_mouse_position() - position).normalized()
+     var muzzle_direction: Vector2 = Vector2.ZERO
      
      # Firing the Gun
-     if Input.is_action_just_pressed("Left Click") and can_shoot:
+     if Input.is_action_pressed("Left Click") and can_shoot:
           var muzzle_markers = muzzle_path
           var selected_muzzle = muzzle_markers[randi()%muzzle_markers.size()]
           can_shoot = false
           $ShootTimer.start()
-          # Emit the Open Fire Signal
-          open_fire.emit(selected_muzzle.global_position, fire_direction)
-          print(fire_direction)
           
+          # Emit the Open Fire Signal
+          open_fire.emit(selected_muzzle.global_position, arm_pointing_direction)
+     
+     # Update the Player Viewing Direction     
+     $"Player Rig".update_facing_direction(mouse_direction)
+     
 # On Shoot Timer Timeout
 func _on_shoot_timer_timeout():
      can_shoot = true
--- Shadow of Aral/Shadow of Aral - New/Object Types/Main Assets/Player/player_rig.gd
+++ Shadow of Aral/Shadow of Aral - New/Object Types/Main Assets/Player/player_rig.gd
@@ -1,7 +1,5 @@
 extends Node2D
 
-#var bullet_scene: PackedScene = preload("res://Object Types/Main Assets/Bullet/bullet.tscn")
-
 # Called when the node enters the scene tree for the first time
 func _ready():
      pass
@@ -9,27 +7,17 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame
 func _process(delta):
-#     $Torso/Head.look_at(get_global_mouse_position())
-#     $Torso/FrontArm/Weapon.look_at(get_global_mouse_position())
+#     pass
      $Torso/FrontArm.look_at(get_global_mouse_position())
-#     $Torso/BackArm.look_at(get_global_mouse_position())
-#     print("Angle = ", $Torso/FrontArm.rotation)
+
      
 func update_animation(animation_name):
      $AnimationPlayer.play(animation_name)
 
 func update_facing_direction(direction):
      if direction.x > 0:
-#          $".".scale = Vector2(1, 1)
-          $Torso.flip_h = false
-
+          $".".scale = Vector2(1, 1)
      elif direction.x < 0:
-#          $".".scale = Vector2(-1, 1)
-          $Torso.flip_h = true
+          $".".scale = Vector2(-1, 1)
 
-          
-#func fire():
-#     var bullet = bullet_scene.instantiate()
-#     bullet.position = $Torso/Weapon.position
-#     add_child(bullet)
-#     print("fire")
+
