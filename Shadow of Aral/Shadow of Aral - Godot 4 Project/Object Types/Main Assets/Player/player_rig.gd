5f0be9dba8301461528f8ac914acfbad16514a20
Prince-Ulric <alex204981157@gmail.com>
2024-06-19 22:28:53 -0400

    Guns and Bandits
    More guns and baddies.

--- Shadow of Aral/Shadow of Aral - Godot 4 Project/Object Types/Main Assets/Player/player_rig.gd
+++ Shadow of Aral/Shadow of Aral - Godot 4 Project/Object Types/Main Assets/Player/player_rig.gd
@@ -18,51 +18,51 @@ var current_recoil: float = 0.0    # Current Recoil
 var max_recoil: float = 10.0       # Max Recoil
 var recoil_increment: float = 0.0  # Increment Recoil
 
-      
+	  
 func _process(_delta):
-      # Set rotation of the arms to mouse position
-      front_arm.look_at(get_global_mouse_position())
+	  # Set rotation of the arms to mouse position
+	  front_arm.look_at(get_global_mouse_position())
 #     pass
-      
+	  
 func update_animation(animation_name):
-      $AnimationPlayer.play(animation_name)
+	  $AnimationPlayer.play(animation_name)
 
 func update_walk_direction(direction):
-      if direction.x > 0:
-            $Torso/BackLeg.flip_h = false
-            $Torso/FrontLeg.flip_h = false
-      elif direction.x < 0:
-            $Torso/BackLeg.flip_h = true
-            $Torso/FrontLeg.flip_h = true
-            
+	  if direction.x > 0:
+			$Torso/BackLeg.flip_h = false
+			$Torso/FrontLeg.flip_h = false
+	  elif direction.x < 0:
+			$Torso/BackLeg.flip_h = true
+			$Torso/FrontLeg.flip_h = true
+			
 func update_facing_direction(direction):
-      if direction.x > 0:
-            $".".scale = Vector2(1, 1)
-      elif direction.x < 0:
-            $".".scale = Vector2(-1, 1)
+	  if direction.x > 0:
+			$".".scale = Vector2(1, 1)
+	  elif direction.x < 0:
+			$".".scale = Vector2(-1, 1)
 
 
 # Received from Player a Command to use Equipped Weapon
 func player_wpn_action(action):
-      
-      # Front Arm Pointing Direction
-      wpn_pointing_direction = Vector2(cos(front_arm.rotation), sin(front_arm.rotation))*($".".scale)
-      
-      # If Player is Shooting the Weapon
-      if action == "shoot":
-      # Check that the Weapon Has Method Shoot
-            if path_to_wpn.has_method("shoot"):
-                  # Call the Shoot Method
-                  path_to_wpn.shoot(wpn_pointing_direction)  
-                  
-      # If Player is Reloading the Weapon
-      elif action == "reload":
-            $AnimationArms.play("Reload")
-            # Check that the Weapon Has Method Shoot
-            if path_to_wpn.has_method("reload"):
-                  # Call the Shoot Method
-                  path_to_wpn.reload()
-                  $Torso/Magazine.z_index = -1
+	  
+	  # Front Arm Pointing Direction
+	  wpn_pointing_direction = Vector2(cos(front_arm.rotation), sin(front_arm.rotation))*($".".scale)
+	  
+	  # If Player is Shooting the Weapon
+	  if action == "shoot":
+	  # Check that the Weapon Has Method Shoot
+			if path_to_wpn.has_method("shoot"):
+				  # Call the Shoot Method
+				  path_to_wpn.shoot(wpn_pointing_direction)  
+				  
+	  # If Player is Reloading the Weapon
+	  elif action == "reload":
+			$AnimationArms.play("Reload")
+			# Check that the Weapon Has Method Shoot
+			if path_to_wpn.has_method("reload"):
+				  # Call the Shoot Method
+				  path_to_wpn.reload()
+				  $Torso/Magazine.z_index = -1
 
 # Arm Recoil Animation
 #func wpn_arm_recoil(wpn_dir):
@@ -74,6 +74,6 @@ func player_wpn_action(action):
 
 # On Animation Finished
 func _on_animation_arms_animation_finished(anim_name):
-      if anim_name == "Reload":
-            $Torso/Magazine.z_index = 1
+	  if anim_name == "Reload":
+			$Torso/Magazine.z_index = 1
 
