90914159b37b29840750feaa84829611205a4dbf
SJohnmith <smith795052@gmail.com>
2024-06-19 23:14:54 -0400

    Things Work Check Indent Settings

--- Shadow of Aral/Shadow of Aral - Godot 4 Project/Object Types/Singleton/EventBus.gd
+++ Shadow of Aral/Shadow of Aral - Godot 4 Project/Object Types/Singleton/EventBus.gd
@@ -6,8 +6,8 @@ signal ui_update()
 
 # Main Events to Handle
 func wpn_fired(muzzle_pos, muzzle_drctn):
-	wpn_shoot_bullets.emit(muzzle_pos, muzzle_drctn)
+     wpn_shoot_bullets.emit(muzzle_pos, muzzle_drctn)
 
 # Player UI
 func update_ui():
-	ui_update.emit()
+     ui_update.emit()
