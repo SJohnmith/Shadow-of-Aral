9fc9dfb0f0b14dba335d999ec8914c942466fe4e
SJohnmith <smith795052@gmail.com>
2024-06-24 22:24:53 -0400

    Hard Reset the Main

--- Shadow of Aral/Shadow of Aral - Godot 4 Project/Globals/globals.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-var bullets = 0
--- Shadow of Aral/Shadow of Aral - Godot 4 Project/Layouts/ui.gd
+++
@@ -1,6 +0,0 @@
-extends CanvasLayer
-
-@onready var bullet_label: Label = $Label
-
-func update_bullet_text():
-     bullet_label.text = "Ammo: " + str(Globals.bullets)
